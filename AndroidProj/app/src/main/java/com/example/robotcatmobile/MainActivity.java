ca4f8ea9669f116ef165c5db09298295450e11ad
sekheng <leesekheng@yahoo.com>
2022-08-18 11:10:26 +0800

    created 2 separate fragments so that we can start working together

--- AndroidProj/app/src/main/java/com/example/robotcatmobile/MainActivity.java
+++ AndroidProj/app/src/main/java/com/example/robotcatmobile/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.robotcatmobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+    HomeFrag homeFragment = new HomeFrag();
+    BluetoothFrag bluetoothFrag = new BluetoothFrag();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.home_tab);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_tab:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+                return true;
+            case R.id.bluetooth_tab:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, bluetoothFrag).commit();
+                return true;
+        }
+        return false;
     }
 }
