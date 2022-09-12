707f0a85dd411ce6cea2270022dbe6b46beb51fd
Kenneth Lee <klee046@e.ntu.edu.sg>
2022-09-13 01:53:42 +0800

    Edited GUI to fit new simulator requirements.

--- Algorithm/SimulatorGUI.py
+++ Algorithm/SimulatorGUI.py
@@ -7,7 +7,7 @@ from Algorithm.SimulatorGrid import SimulatorGrid
 class Robot_Cats_Simulator_App():
     def __init__(self):
         self.APP_WIDTH = 1280
-        self.APP_HEIGHT = 960
+        self.APP_HEIGHT = 915
         self.GRID_SIZE = 850
 
         self.main_app = None
@@ -37,12 +37,12 @@ class Robot_Cats_Simulator_App():
         with dpg.window(label="Robot Cats Simulator") as self.main_app:
             with dpg.child_window(label="Grid", height=self.GRID_SIZE, width=self.GRID_SIZE, border=True, no_scrollbar=True, pos = [0,20]) as self.grid_app:
                 pass
-            with dpg.child_window(label="Controls", width=450, pos=[self.GRID_SIZE+10, 20]) as self.control_app:
-                dpg.add_input_text(tag='instructions_header', default_value="How to set/remove/edit obstacles", readonly=True)
-                dpg.add_text("LClick on an empty cell to set obstacle")
-                dpg.add_text("LClick on an obstacle to change direction")
-                dpg.add_text("RClick on a cell to clear cell")
-            with dpg.child_window(label='Robot Info', pos=[self.GRID_SIZE+10, 125]) as robot_info_app:
+            # with dpg.child_window(label="Controls", width=450, pos=[self.GRID_SIZE+10, 20]) as self.control_app:
+            #     dpg.add_input_text(tag='instructions_header', default_value="How to set/remove/edit obstacles", readonly=True)
+            #     dpg.add_text("LClick on an empty cell to set obstacle")
+            #     dpg.add_text("LClick on an obstacle to change direction")
+            #     dpg.add_text("RClick on a cell to clear cell")
+            with dpg.child_window(label='Robot Info', pos=[self.GRID_SIZE+10, 20]) as robot_info_app:
                 dpg.add_input_text(tag='robot_info_header', default_value="Robot Information", readonly=True)
                 dpg.add_text("Robot Position:")
                 with dpg.group(horizontal=True):
@@ -53,16 +53,11 @@ class Robot_Cats_Simulator_App():
                 with dpg.group(horizontal=True):
                     dpg.add_text('Robot Direction:')
                     dpg.add_input_text(tag="robot_direction_highlight", width=45, default_value='North', readonly=True)
-            with dpg.child_window(label='Controls', pos=[self.GRID_SIZE+10,225]):
-                dpg.add_input_text(tag='controls_header', width=65, default_value='Controls', readonly=True)
-                dpg.add_text('')
-                dpg.add_checkbox(tag='step_by_step', label='Step-by-step')
-                with dpg.group(horizontal=True):
-                    dpg.add_button(tag='start', label='Start', width=175, height=50)
-                    dpg.add_button(tag='reset', label='Reset', width=175, height=50)
-            with dpg.child_window(label='console', pos=[self.GRID_SIZE+10, 360]):
+            with dpg.child_window(label='Controls', pos=[self.GRID_SIZE+10,125]):
+                dpg.add_button(tag='start', label='Start', width=375, height=50)
+            with dpg.child_window(label='console', pos=[self.GRID_SIZE+10, 190]):
                 dpg.add_input_text(tag='console_header', width=57, default_value='Console', readonly=True)
-                dpg.add_input_text(tag='console_body', multiline=True, default_value='', width=400, height=1000, readonly=True)
+                dpg.add_input_text(tag='console_body', multiline=True, default_value='', width=400, height=650, readonly=True)
 
         self.pathing = SimulatorManager(self.grid_app)
 
